d5f46c4d7cb2b1a113bd43d48d1c0b5c60dd0ed7
tajaa <tajatheprince@gmail.com>
2024-06-28 23:06:53 -0700

    now I have my categories

--- todo.c
+++ todo.c
@@ -7,6 +7,29 @@
 static int winw = 1280, winh = 720;
 static LfFont titlefont;
 
+static void rendertopbar() {
+  lf_push_font(&titlefont);
+  lf_text("Task List:");
+  lf_pop_font();
+
+  {
+    const float width = 160.0f;
+
+    lf_set_ptr_x_absolute(winw - width - WIN_MARGIN * 2.0f);
+    LfUIElementProps props = lf_get_theme().button_props;
+    props.margin_left = 0.0f;
+    props.margin_right = 0.0f;
+    props.color = (LfColor){65, 167, 204, 255};
+    props.border_width = 0.0f;
+    props.corner_radius = 4.0f;
+    lf_push_style_props(props);
+    lf_set_line_should_overflow(false);
+    lf_button_fixed("new task", width, -1);
+    lf_set_line_should_overflow(true);
+    lf_pop_style_props();
+  }
+}
+
 int main() {
   glfwInit();
 
@@ -30,22 +53,22 @@ int main() {
     lf_div_begin(((vec2s){WIN_MARGIN, WIN_MARGIN}),
                  ((vec2s){winw - WIN_MARGIN * 2.0f, winh - WIN_MARGIN * 2.0f}),
                  true);
-
-    lf_push_font(&titlefont);
-    lf_text("Get Done:");
-    lf_pop_font();
-
+    rendertopbar();
+    lf_next_line();
     {
-      const float width = 160.0f;
+      const uint32_t numfilters = 6;
 
-      lf_set_ptr_x_absolute(winw - width - WIN_MARGIN * 2.0f);
-      LfUIElementProps props = lf_get_theme().button_props;
-      props.margin_left = 0.0f;
-      props.margin_right = 0.0f;
-      lf_push_style_props(props);
-      lf_set_line_should_overflow(false);
-      lf_button_fixed("new task", width, -1);
-      lf_pop_style_props();
+      static const char *filters[] = {"all", "in progress", "completed",
+                                      "low", "medium",      "high"};
+      for (uint32_t i = 0; i < numfilters; i++) {
+        LfUIElementProps props = lf_get_theme().text_props;
+        props.margin_top = 20.0f;
+        props.margin_right = 30.0f;
+        lf_push_style_props(props);
+
+        lf_button(filters[i]);
+        lf_pop_style_props();
+      }
     }
 
     lf_pop_font();
